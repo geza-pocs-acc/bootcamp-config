6a9b43d07a493569a15fb118a1ba3264457c4b49
geza <geza.pocs@cognite.com>
2023-06-15 08:57:41 +0100

    created mainfests and transformations

---
+++ config/transformations/ts_assets_mapping/transformation.sql
@@ -0,0 +1,6 @@
+/* MAPPING_MODE_ENABLED: false */
+  SELECT ts.externalId AS externalId,
+         ast.id AS assetId
+  FROM `_cdf`.`timeseries` AS ts
+  INNER JOIN `_cdf`.`assets` AS ast
+    ON split(ts.externalId, ":") [0] = ast.externalId
